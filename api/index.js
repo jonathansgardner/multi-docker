dcce51daa715dc55a2350602d82d8ff980e4062f
Jonathan Gardner <jgard714@gmail.com>
2019-11-10 14:58:02 -0500

    fixed server to not accept indexes over 40

--- api/index.js
+++ api/index.js
@@ -54,7 +54,8 @@ app.get('/values/current', async (req, res) => {
 
 app.post('/values', async (req, res) => {
   const index = req.body.index;
-  if (parseInt(index) > 40) res.status(422).send('Index too high');
+  if (parseInt(index) > 40)
+    return res.status(422).send('Index too high');
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
